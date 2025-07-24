1215fdecf261e8b7af59bd25cc15bb851f18834e
Nathalia <nmsd1809@hotmail.com>
2025-07-24 14:23:19 -0500

    Update switch_structure.c

--- chapter03/switch_structure.c
+++ chapter03/switch_structure.c
@@ -1,30 +1,32 @@
-/*switch*/
+/* switch*/
 
+#include <stdio.h>
 
-#include<stdio.h>
-
-int main()
-{
+int main() {
     int option;
 
     printf("\n1 \tYaya Beach\n");
-    printf("\n2 \tEl fuerte\n");
-    printf("\n3 \tHayhu\n");
+    printf("2 \tEl fuerte\n");
+    printf("3 \tHayhu\n");
 
-    printf("\nchoose a dinner option: ");
-    scanf("%d",&option);
+    printf("\nChoose a dinner option: ");
+    scanf("%d", &option);
 
-    switch (option){
-    case 1:
-    printf("\nYou booked for Yaya Beach\n");
-    break;
-    case 2:
-    printf("\nYou booked for El fuerte\n");
-    break;
-    case 3:
-    printf("\nYou booked for Hayhu\n");
-    break;
+    switch (option) {
+        case 1:
+            printf("\nYou booked for Yaya Beach\n");
+            break;
+        case 2:
+            printf("\nYou booked for El fuerte\n");
+            break;
+        case 3:
+            printf("\nYou booked for Hayhu\n");
+            break;
 
+        default:
+            printf("\nInvalid option. Please choose 1, 2, or 3.\n");
     }
+
     return 0;
 }
+
