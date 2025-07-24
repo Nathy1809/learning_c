596296f196cd4f4544877686a648564ce9f47461
Nathalia <nmsd1809@hotmail.com>
2025-07-23 20:21:49 -0500

    Create chapter1.c

---
+++ chapter01/chapter1.c
@@ -0,0 +1,17 @@
+/* C Programming for the Absolute Beginner */
+#include <stdio.h>
+//comentario
+int main()
+{
+    printf("\nC you later\n");
+    printf("nathalia\nmaria\nsoledad\ndominguez\n");
+    printf("nathalia ");
+    printf("maria ");
+    printf("soeldad ");
+    printf("dominguez\n");
+    printf("nena\\nene\n");
+    printf("Esta secuencia de escape mueve el cursor ");
+    
+
+    return 0;
+}
