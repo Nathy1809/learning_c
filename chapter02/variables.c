f7ec27ae6b4fb79fff11c632b3c2994d68429514
Nathalia <nmsd1809@hotmail.com>
2025-07-23 20:27:33 -0500

    Create variables.c

---
+++ chapter02/variables.c
@@ -0,0 +1,15 @@
+/*variables*/
+#include<stdio.h>
+
+#include<stdio.h>
+int main ()
+{
+    int dia;
+    char mes;
+    dia = 10;
+    mes = '10';
+
+    printf("La fecha de la boda es dia %d del mes %s", dia, mes);
+
+    return 0;
+}
