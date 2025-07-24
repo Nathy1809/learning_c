b3ce5f115097606e935ddea4269f23c325e0bb96
Nathalia <nmsd1809@hotmail.com>
2025-07-24 18:10:58 -0500

    Update excercise_2.c

--- chapter04/excercise_2.c
+++ chapter04/excercise_2.c
@@ -1,7 +1,7 @@
 /*
-*Make a function that asks the user to enter one of the sides of the triangle, that validates that the side of the triangle is greater than 0,
-*(if the user enters 0 or less, it asks the user to enter it again), and returns the value.
-*/
+ *Make a function that asks the user to enter one of the sides of the triangle, that validates that the side of the triangle is greater than 0,
+ *(if the user enters 0 or less, it asks the user to enter it again), and returns the value.
+ */
 
 #include<stdio.h>
 
@@ -11,11 +11,11 @@ int main(int argc, char *argv[])
   int num_user;
   int magic_num = 6;
 
-  do{
+  do {
     printf("\nGuess the magic number between 1 and 10: ");
     scanf("%d", &num_user);
 
-    if(num_user != magic_num ){
+    if (num_user != magic_num ){
         printf("You're out of luck. Try again.\n");
     }
   }
