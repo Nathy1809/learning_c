a873cf4b5f3e7d05b5510c6b9e7df4254ec7488d
Nathalia <nmsd1809@hotmail.com>
2025-07-24 11:50:14 -0500

    Create switch_structure.c

---
+++ chapter03/switch_structure.c
@@ -0,0 +1,30 @@
+/*switch*/
+
+
+#include<stdio.h>
+
+int main()
+{
+    int option;
+
+    printf("\n1 \tYaya Beach\n");
+    printf("\n2 \tEl fuerte\n");
+    printf("\n3 \tHayhu\n");
+
+    printf("\nchoose a dinner option: ");
+    scanf("%d",&option);
+
+    switch (option){
+case 1:
+    printf("\nYou booked for Yaya Beach\n");
+    break;
+case 2:
+    printf("\nYou booked for El fuerte\n");
+    break;
+case 3:
+    printf("\nYou booked for Hayhu\n");
+    break;
+
+    }
+    return 0;
+}
