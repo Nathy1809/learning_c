7d0e83b435099a735d07cfa3e2dc923d556e5a78
Nathalia <nmsd1809@hotmail.com>
2025-07-24 19:11:36 -0500

    Update exercise_6.c

--- chapter04/exercise_6.c
+++ chapter04/exercise_6.c
@@ -1,1 +1,15 @@
+/*
+ *Modify exercise 5 so that you do not use the continue,
+ *and you go from 1 to 10 but in the for you are going to increase by two units instead of 1 unit
+ */
 
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+   for (int i = 1; i <= 10; i += 2) {
+    printf("%d \n", i);
+   }
+
+  return 0;
+}
