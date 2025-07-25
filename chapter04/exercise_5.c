1571945aba6e3b4a2d55f616983e370f323637ce
Nathalia <nmsd1809@hotmail.com>
2025-07-24 19:04:39 -0500

    Update exercise_5.c

--- chapter04/exercise_5.c
+++ chapter04/exercise_5.c
@@ -1,1 +1,17 @@
+/*
+ *Loop through 1 to 10 in a for loop, print the odd numbers on the screen, skip the even numbers using continue.
+ */
 
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+  for (int i = 1; i <= 10; i++){
+    if (i % 2 == 0){
+      continue;
+      }
+      printf("%d \n", i);
+  }
+
+  return 0;
+}
